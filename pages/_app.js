83704c5e0fa7c4e1615bdf47f3e6b3cee0370d51
Chris Mittell <mittell@hotmail.co.uk>
2022-09-02 13:52:04 +0900

    feat: added Toolbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import ToolBar from '../components/ToolBar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ToolBar />
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
